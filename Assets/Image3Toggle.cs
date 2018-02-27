8de01b8b6f8ec2640abc993317a0db012244df1a
Hemkinator <mhemker@UCSD.EDU>
2018-02-27 14:48:33 -0800

    Init Commit

---
+++ Assets/Image3Toggle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Image3Toggle : MonoBehaviour, ITrackableEventHandler {
+	private TrackableBehaviour mTrackableBehaviour;
+	// Use this for initialization
+	void Start () {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour) {
+			mTrackableBehaviour.RegisterTrackableEventHandler (this);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{ 
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			OnTrackingFound ();
+		} else {
+			OnTrackingLost ();
+		}
+	} 
+
+	public void OnTrackingFound(){
+		GameObject.Find ("Instantiator").GetComponent<InstantiationScript> ().image3tracked = true;
+	}
+	public void OnTrackingLost(){
+		GameObject.Find ("Instantiator").GetComponent<InstantiationScript> ().image3tracked = false;
+	}
+
+}
---
+++ Assets/InstantiationScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiationScript : MonoBehaviour {
+	public bool image1tracked = false;
+	public bool image2tracked = false;
+	public bool image3tracked = false;
+	public bool image4tracked = false;
+	public GameObject image1;
+	public GameObject image2;
+	public GameObject image3;
+	public GameObject image4;
+	public Transform Testcube;
+	private bool complete = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 pos1 = Vector3.Lerp (image1.transform.position, image2.transform.position, 0.5f);
+		Vector3 pos2 = Vector3.Lerp (image3.transform.position, image4.transform.position, 0.5f);
+		Vector3 finalpos = Vector3.Lerp (pos1, pos2, 0.5f);
+        Testcube.transform.position = finalpos;
+	}
+
+}
